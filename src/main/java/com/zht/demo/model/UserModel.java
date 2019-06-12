455ae95cb01d476590a75894f3fce0fa28559c05
庄浩棠 <aiuzht119@163.com>
2019-06-12 20:04:27 +0800

    创建UserModel

---
+++ src/main/java/com/zht/demo/model/UserModel.java
@@ -0,0 +1,39 @@
+package com.zht.demo.model;
+
+/**
+ * @Auther: ZHUANGHAOTANG
+ * @Date: 2019/6/12 20:03
+ * @Description:
+ */
+public class UserModel {
+
+    private Integer id;
+
+    private String username;
+
+    private String password;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
